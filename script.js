5ff7e6699ea945527882cbc67866361e5c3735b2
Rossell149 <rossellreyes04@gmail.com>
2025-01-24 20:23:43 -0600

    add web pages

---
+++ script.js
@@ -0,0 +1,6 @@
+ocument.getElementById('togglePassword').addEventListener('click', function () { 
+    const passwordField = document.getElementById('password');
+    const type = passwordField.type === 'password' ? 'text' : 'password';
+    passwordField.type = type;
+    this.textContent = type === 'password' ? '👁️' : ' 🙈';
+});
